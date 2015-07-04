5097eff387287343b860ce10902fa2281fb57c19
Kyra Fillmore <kfillmore27@gmail.com>
2015-07-03 20:02:16 -0400

    Change storage location for carrierwave

--- app/uploaders/background_image_uploader.rb
+++ app/uploaders/background_image_uploader.rb
@@ -11,8 +11,8 @@ class BackgroundImageUploader < CarrierWave::Uploader::Base
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
-  def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  def cache_dir
+    "#{Rails.root}/tmp/uploads"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
