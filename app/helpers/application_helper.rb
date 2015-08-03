6be69db5d3a8107d0b3fce3d5c16fd0c727a651b
Kyra Fillmore <kfillmore27@gmail.com>
2015-08-02 23:39:26 -0400

    background images on thumbnails working... needs tweaking

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,4 @@ module ApplicationHelper
     "background: url(#{@profile.background_image.to_s}) no-repeat fixed left top;
     max-height: 300px;"
   end
-  def show_brand_background_thumbnail
-    @profile.background_image(:thumb)
-  end
 end
