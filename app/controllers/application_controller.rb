099110d428b51bd44ebe61ae0e1814cc2efb089f
Todd Metheny <todd.metheny@gmail.com>
2015-08-12 00:08:05 -0400

    .

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,5 +11,9 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << :name
+
+    devise_parameter_sanitizer.for(:account_update) { |u| 
+      u.permit(:password, :password_confirmation, :current_password) 
+    }
   end
 end
