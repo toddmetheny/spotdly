a35bc0d40505a7ea3c09c4c08be08dfc3502c84b
Todd Metheny <todd.metheny@gmail.com>
2015-08-12 21:51:40 -0400

    trying to get default background image to render

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,4 +5,4 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w( ProximaNova-Light.ttf ProximaNova-Thin.ttf ProximaNovaAlt-Light.otf )
+Rails.application.config.assets.precompile += %w( .svg .eot .woff .ttf .png .jpg .gif )
