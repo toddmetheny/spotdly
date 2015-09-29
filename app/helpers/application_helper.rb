68d74cf93528984c1b74a435e0d9c13634f2d3cf
Kyra Fillmore <kfillmore27@gmail.com>
2015-09-29 17:29:51 -0400

    Adjusting background images

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,10 @@
 module ApplicationHelper
   def show_brand_background
     unless @profile.background_image.blank?
-      "background: url(#{@profile.background_image.to_s}) no-repeat fixed left top;
+      "background: url(#{@profile.background_image.to_s}) fixed center top;
        max-height: 300px;"
     else
-      "background: url('/assets/background.png') no-repeat fixed left top;
+      "background: url('/assets/background.png') fixed center top;
        max-height: 300px;"
     end
   end
