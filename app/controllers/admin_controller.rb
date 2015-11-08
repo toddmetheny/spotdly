c5280581514651f616a797abb95330c6e5b87172
Todd Metheny <todd.metheny@gmail.com>
2015-11-08 17:49:17 -0500

    .

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,6 +1,6 @@
 class AdminController < ApplicationController
   def admin
-    unless current_user.admin
+    unless current_user && current_user.admin
       redirect_to root_path
     end
     @users = User.all
