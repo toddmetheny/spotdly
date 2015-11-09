ce14a7e2dfdf8e966852778c6e91e3bd6b125262
Todd Metheny <todd.metheny@kipusystems.com>
2015-11-09 11:59:34 -0500

    removing cache control header

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -7,12 +7,12 @@ CarrierWave.configure do |config|
     # :provider               => 'AWS',                        # required
     # :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],                 # required
     # :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],             # required
-    :access_key_id      => ENV['AWS_ACCESS_KEY_ID'],                 # required
-    :secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],             # required
+    access_key_id: ENV['AWS_ACCESS_KEY_ID'],                 # required
+    secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],             # required
     region: 'us-east-1'
 
   }
   config.aws_bucket  = ENV['S3_BUCKET_NAME']                             # required
   config.aws_acl    = 'public-read'                                 # optional, defaults to true
-  config.aws_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
+  # config.aws_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
 end
